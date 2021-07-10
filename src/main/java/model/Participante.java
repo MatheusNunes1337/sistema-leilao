83716c77d7466be83bf3ceec7356cfab76fe78bd
Matheus <matheusyagami147@gmail.com>
2021-07-10 19:19:48 -0300

    criada a classe de participante e seus atributos básicos

---
+++ src/main/java/model/Participante.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Participante {
+	private int id;
+	private String nome;
+	private String login;
+	private String senha;
+	private String endereco;
+	private String telefone;
+	
+	
+}
