32cc094d7bf195f98b1bd64fcda8611023428e49
Matheus <matheusyagami147@gmail.com>
2021-07-10 19:28:29 -0300

    criada a classe de lance e adicionada na classe de participante. Classe participante com getters e setters

---
+++ src/main/java/model/Lance.java
@@ -0,0 +1,9 @@
+package model;
+
+import java.time.LocalTime;
+
+public class Lance {
+	private double valorLance;
+	private LocalTime horaLance;
+	private Participante participante;
+}
--- src/main/java/model/Participante.java
+++ src/main/java/model/Participante.java
@@ -1,5 +1,8 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Participante {
 	private int id;
 	private String nome;
@@ -7,6 +10,82 @@ public class Participante {
 	private String senha;
 	private String endereco;
 	private String telefone;
+	private List<Participante> lances = new ArrayList<>();
 	
+	public Participante() {
+		super();
+	}
+
+	public Participante(int id, String nome, String login, String senha, String endereco, String telefone) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.login = login;
+		this.senha = senha;
+		this.endereco = endereco;
+		this.telefone = telefone;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public List<Participante> getLances() {
+		return lances;
+	}
+
+	public void setLances(List<Participante> lances) {
+		this.lances = lances;
+	}
+
+	@Override
+	public String toString() {
+		return "Participante [id=" + id + ", nome=" + nome + ", login=" + login + ", senha=" + senha + ", endereco="
+				+ endereco + ", telefone=" + telefone + ", lances=" + lances + "]";
+	}
 	
 }
